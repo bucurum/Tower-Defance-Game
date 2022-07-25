0c92c49b46bc97ef33da0db62ae68d6de200e80c
bucurum <bucurum18@posta.pau.edu.tr>
2022-07-25 17:45:10 +0300

    Making playground, enemy mover script, tmp, waypoint and coordinatelabler script

---
+++ Assets/[Game]/Scripts/EnemyMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path = new List<WayPoint>();
+    [SerializeField] [Range(0,5f)] float speed = 1f;
+
+    void Start()
+    {
+        StartCoroutine(PrintWaypointName());     
+    }
+
+    IEnumerator PrintWaypointName()
+    {
+        foreach (WayPoint waypoint in path)
+        {
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
